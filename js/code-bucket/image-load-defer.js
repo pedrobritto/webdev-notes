7a15f3312a9b64224a4940a99bd455a290a5aacc
Pedro Britto <pedroivobritto@gmail.com>
2016-11-16 08:59:00 -0300

    Create image-load-defer.js

---
+++ js/code-bucket/image-load-defer.js
@@ -0,0 +1,13 @@
+/**
+ * Defers image load in a website
+ *
+ */
+
+function init() {
+  var imgDefer = document.getElementsByTagName('img');
+  for (var i=0; i<imgDefer.length; i++) {
+    if(imgDefer[i].getAttribute('data-src')) {
+      imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+    }
+  }
+} window.onload = init;
