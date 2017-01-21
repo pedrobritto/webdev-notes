ca702941e86edaf10a17f06c6280580d3b6272ec
Pedro Britto <pedroivobritto@gmail.com>
2017-01-21 11:03:29 -0300

    changed gulp folder and update code

--- src/gulp/sass+browsersync.js
+++ src/gulp/sass+browsersync.js
@@ -1,27 +1,25 @@
-// Gulpfile example for a simple web development workflow
-
+// Gulpfile for a simple web development workflow
 
 // Requires
-var gulp = require('gulp'),
-		sass = require('gulp-sass'),
+var gulp        = require('gulp'),
+		sass        = require('gulp-sass'),
 		browserSync = require('browser-sync').create();
 
 // Paths
-var input		= './app/sass/main.sass',
-		inputAll= './app/sass/**/*.sass',
+var input		= './app/scss/**/*.scscc',
 		output	= './app/css';
 
 // General Tasks
 gulp.task('sass', function() {
 	return gulp.src(input)
-	.pipe(sass({outputStyle: 'expanded'}).on('error', logError))
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 	.pipe(gulp.dest(output))
 	.pipe(browserSync.reload({
 		stream: true
 	}))
 });
 
-gulp.task('browserSync', function() {
+gulp.task('sync', function() {
 	browserSync.init({
 		server: {
 			baseDir: 'app'
@@ -34,8 +32,8 @@ gulp.task('sass:watch', function() {
 	gulp.watch(inputAll, ['sass']);
 });
 
-gulp.task('livereload', ['browserSync', 'sass'], function() {
-	gulp.watch(inputAll, ['sass']);
-	gulp.watch('./app/*.html', browserSync.reload);
+gulp.task('default', ['sync', 'sass'], function() {
+	gulp.watch(input, ['sass']);
+	gulp.watch('./app/**/*.html', browserSync.reload);
 	gulp.watch('./app/js/**/*.js', browserSync.reload);
 })
